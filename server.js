f34049f73219bf2f8905f31ddaf505058e84bdee
rajdhanwar-suraj <sharmasuraj747947@gmail.com>
2024-06-28 18:03:44 +0530

    fix cors

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
 const mysql = require('mysql2');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
 const db = mysql.createConnection({
